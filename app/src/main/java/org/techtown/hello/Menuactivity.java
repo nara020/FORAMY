49c04cfce9141bca4437c6b957b395d5576b87da
nara020 <nara020@naver.com>
2019-04-30 16:07:17 +0900

    11

---
+++ app/src/main/java/org/techtown/hello/Menuactivity.java
@@ -0,0 +1,19 @@
+package org.techtown.hello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class Menuactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menuactivity);
+    }
+    public void onBackButtonClicked(View v) {
+        Toast.makeText(getApplicationContext(), "돌아가기 버튼을 눌렀어요.", Toast.LENGTH_LONG).show();
+        finish();
+    }
+}
